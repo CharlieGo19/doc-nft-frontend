d757152cfcc274479a196db564fa04d76b7092ab
Charlie <amaise@live.com>
2023-01-18 10:34:45 +0000

    Testing config changes.

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
         adapter: adapter({
             pages: "build",
             assets: "build",
-            fallback: "index.html",
+            fallback: "200.html",
         }),
         prerender: {
             entries: ["*"],
